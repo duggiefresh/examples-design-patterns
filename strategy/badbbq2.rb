12b12cff912864039dce3d4322b3071b5ec0738a
Doug Yun <yun.douglas@gmail.com>
2013-07-22 22:55:45 -0400

    [Strategy] Sweet little example

---
+++ strategy/badbbq2.rb
@@ -0,0 +1,39 @@
+class Grill
+  attr_accessor :food
+
+  def initialize food
+    @food = food
+  end
+
+  def fired_up?
+    true
+  end
+
+  def grill
+    "Grilling the #{@food.type}!"
+  end
+end
+
+class Food
+  def type
+    raise NotImplementedError, 'Ask the subclass'
+  end
+end
+
+class HotDog < Food
+  def type
+    'hot dogs'
+  end
+end
+
+class Hamburger < Food
+  def type
+    'hamburgers'
+  end
+end
+
+class Veggie < Food
+  def type
+    'veggie patties'
+  end
+end
