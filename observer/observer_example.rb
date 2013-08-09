def10e7543006b2d49c8f34a2bd9f6d734783e89
Doug Yun <yun.douglas@gmail.com>
2013-08-09 16:02:25 -0400

    Observer example 2.

---
+++ observer/observer_example.rb
@@ -0,0 +1,44 @@
+class Employee
+  attr_reader :name
+  attr_accessor :title, :salary
+
+  def initialize name, title, salary
+    @name = name
+    @title = title
+    @salary = salary
+    @observers = []
+  end
+
+  def salary=(new_salary)
+    @salary = new_salary
+    notify_observers
+  end
+
+  def notify_observers
+    @observers.each do |observer|
+      observer.update(self)
+    end
+  end
+
+  def add_observer observer
+    @observers << observer
+  end
+
+  def delete_observer observer
+    @observers.delete observer
+  end
+end
+
+class Payroll
+  def update changed_employee
+    puts "Cut a new check for #{changed_employee.name}!"
+    puts "The new salary is #{changed_employee.salary}!"
+  end
+end
+
+class TaxMan
+  def update changed_employee
+    puts "Send #{changed_employee.name} a new tax bill!"
+
+  end
+end
