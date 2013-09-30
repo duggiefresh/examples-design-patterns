c4dbabd20d659f1ff7b4026b13bd39bd9f66e945
Doug Yun <yun.douglas@gmail.com>
2013-09-30 19:56:49 -0400

    Updated examples

--- composite/coffeemaker.rb
+++ composite/coffeemaker.rb
@@ -1,40 +1,49 @@
-class MorningRoutine
+class CoffeeRoutine
   attr_reader :name
 
   def initialize(name)
     @name = name
   end
 
-  def time_required
+  def time
     0.0
   end
 end
 
-class GrindCoffee < MorningRoutine
+class GrindCoffee < CoffeeRoutine
   def initialize
-    super('Grinding some coffee!')
+    super 'Grinding some coffee!'
   end
 
-  def time_required
+  def time
     0.5
   end
 end
 
-class BoilWater < MorningRoutine
+class BoilWater < CoffeeRoutine
   def initialize
-    super('Boiling some water!')
+    super 'Boiling some water!'
   end
 
-  def time_required
+  def time
     4.0
   end
 end
 
-class CompositeTask < MorningRoutine
+class AddCoffee < CoffeeRoutine
+  def initialize
+    super 'Adding in the coffee!'
+  end
+
+  def time
+    1.0
+  end
+end
+
+class CompositeTasks < CoffeeRoutine
   attr_reader :tasks
 
   def initialize(name)
-    super(name)
     @tasks = []
   end
 
@@ -47,16 +56,31 @@ class CompositeTask < MorningRoutine
   end
 
   def time_required
-    time = 0.0
-    tasks.each { |task| time += task.time_required }
-    time
+    total_time = 0.0
+    tasks.each { |task| total_time += task.time }
+    total_time
   end
 end
 
-class FrenchPress < CompositeTask
+class FrenchPress < CompositeTasks
   def initialize
-    super('Making some coffee with the French press')
-    add_sub_task(GrindCoffee.new)
-    add_sub_task(BoilWater.new)
+    super 'Using the FrenchPress to make coffee!!!'
+    add_sub_task GrindCoffee.new
+    add_sub_task BoilWater.new
+    add_sub_task AddCoffee.new
+    # ... Omitted actual steps to make coffee from a French press ...
+    # ... Imagine PressPlunger class has been defined already ...
+    #add_sub_task PressPlunger.new
+  end
+end
+
+class DripMaker < CompositeTasks
+  def initialize
+    super 'Using the DripMaker to make coffee!!!'
+    add_sub_task GrindCoffee.new
+    add_sub_task BoilWater
+    add_sub_task AddCoffee.new
+    # ... Imagine PressStartButton class has been defined already ...
+    add_sub_task PressStartButton.new
   end
 end
